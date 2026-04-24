3d6f8a7f25fa84a9d4393c98c7ce59be0bfdc6ae
Sherlyrhmaa <sherlypuput04@gmail.com>
2026-04-24 09:45:39 +0700

    Perbaiki CORS dengan environment variables

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,17 +5,11 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:1337',
-        'https://kinerja-dosen-frontend.vercel.app',
-        'https://young-hero-905ddfaf2b.strapiapp.com'
-      ],
+      origin: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : ['http://localhost:3000', 'http://localhost:1337'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
       credentials: true,
-      keepHeadersOnError: true
+      keepHeadersOnError: true,
     },
   },
   'strapi::poweredBy',
