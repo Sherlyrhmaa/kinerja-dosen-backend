4ed2262c2bd4ecc785bd7e9d8138e64b989f7dc4
Sherlyrhmaa <sherlypuput04@gmail.com>
2026-04-24 09:14:53 +0700

    Perbaiki konfigurasi CORS untuk frontend Vercel

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,26 +1,21 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
-          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      headers: '*',
-      origin: ['http://localhost:3000', 'https://kinerja-dosen-frontend.vercel.app'],
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:1337',
+        'https://kinerja-dosen-frontend.vercel.app',
+        'https://young-hero-905ddfaf2b.strapiapp.com'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
+      credentials: true,
+      keepHeadersOnError: true
     },
   },
   'strapi::poweredBy',
